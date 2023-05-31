38a5c77a2b8b99c00b58cf10964c970010293f1d
REPANA JYOTHI PRAKASH <98946604+REPANAJYOTHIPRAKASH629@users.noreply.github.com>
2023-05-31 12:05:46 +0530

    6b.c

--- 6b.c
+++ 6b.c
@@ -26,67 +26,49 @@ Child id : 6542
 
 #include<stdio.h>
 #include<unistd.h>
-void main()
+#include<sys/wait.h>
+int main()
 {
 int pid;
+printf("welcome\n");
 pid=fork();
-printf("welcome");
+
+
 if(pid==0)
 {
-printf("child");
-execl("/bin/ls","ls","-l",(char*)0);
-printf("hello\n");
+printf("child :%d\n",getpid());
+printf("%d",getpid());
+execl("/bin/ls","ls",(char*)0);
+
+
 }
 else
 {
-wait(0);
-printf("parent");
-execl("/bin/ls","ls","-l",(char*)0);
+wait(NULL);
+printf("parent :%d\n",getpid());
+execl("/bin/ls","ls","-f",(char*)0);
+
 }
+return 0;
 }
+
 
 
 /*output*/
 
-total 1388
--rw-rw-r-- 1 22695a3203 22695a3203       0 Apr  1 10:15 1
--rw-rw-r-- 1 22695a3203 22695a3203 1286144 Apr  1 10:13 123.sh
--rw-rw-r-- 1 22695a3203 22695a3203       0 Apr 12 11:02 2b,sh
--rw-rw-r-- 1 22695a3203 22695a3203     123 Apr 12 11:37 2b.sh
--rw-rw-r-- 1 22695a3203 22695a3203     254 May 24 11:17 6a.c
--rwxrwxr-x 1 22695a3203 22695a3203    7081 May 24 11:17 a.out
--rw-rw-r-- 1 22695a3203 22695a3203     217 May 24 10:32 env.c
--rw-rw-r-- 1 22695a3203 22695a3203     101 Apr  1 09:52 evenodd.sh
--rw-rw-r-- 1 22695a3203 22695a3203     120 Apr  1 10:15 factorial.sh
--rw-rw-r-- 1 22695a3203 22695a3203     810 May 10 11:14 fcfs.c
--rw-rw-r-- 1 22695a3203 22695a3203     923 Apr 12 11:50 filenames.sh
--rw-rw-r-- 1 22695a3203 22695a3203       0 Mar 29 10:43 hell01.java
--rw-rw-r-- 1 22695a3203 22695a3203     116 Mar 29 10:46 hello1.java
--rw-rw-r-- 1 22695a3203 22695a3203      84 Apr 12 11:21 hello.c
--rw-rw-r-- 1 22695a3203 22695a3203      93 Apr  1 10:51 help.sh
--rw-rw-r-- 1 22695a3203 22695a3203     261 May  2 11:46 multipleopens.sh
-drwxrwxr-x 2 22695a3203 22695a3203    4096 Apr 12 11:04 new
--rw-rw-r-- 1 22695a3203 22695a3203     414 Mar 29 10:46 one.class
--rw-rw-r-- 1 22695a3203 22695a3203     127 Mar 29 11:57 one.sh
--rw-rw-r-- 1 22695a3203 22695a3203     215 May  3 09:50 permission.sh
--rw-rw-r-- 1 22695a3203 22695a3203      84 Apr  1 12:03 power.sh
--rw-rw-r-- 1 22695a3203 22695a3203    2344 May 10 11:14 priority.c
--rw------- 1 22695a3203 22695a3203    2341 May 10 12:07 priority.c.save
--rw------- 1 22695a3203 22695a3203    2341 May 10 12:07 priority.c.save.1
--rw-rw-r-- 1 22695a3203 22695a3203    1084 May 24 10:09 rr.c
--rw-rw-r-- 1 22695a3203 22695a3203     316 Apr 12 11:58 salary.sh
--rw-rw-r-- 1 22695a3203 22695a3203      25 May  2 11:48 sample1.txt
--rw-rw-r-- 1 22695a3203 22695a3203       0 Apr 12 09:55 sample.txt
--rw-rw-r-- 1 22695a3203 22695a3203    1237 May 10 10:42 sjf.c
--rw-rw-r-- 1 22695a3203 22695a3203      44 Apr  1 09:23 s.txt
--rw-rw-r-- 1 22695a3203 22695a3203     411 Mar 29 10:54 two.class
--rw-rw-r-- 1 22695a3203 22695a3203     118 Mar 29 10:53 two.java
--rw-rw-r-- 1 22695a3203 22695a3203     101 Apr  1 09:51 twoo.sh
--rw-rw-r-- 1 22695a3203 22695a3203     208 Apr 12 10:00 uppercase_filenames.sh
--rw-rw-r-- 1 22695a3203 22695a3203      22 Mar 29 11:42 world1.py
--rw-rw-r-- 1 22695a3203 22695a3203      10 Mar 29 11:45 world.py
-factorial.sh  fcfs.c       permission.sh  ..           hello.c      rr.c        evenodd.sh    123.sh            .priority.c.swp         one.sh             two.class
-2b,sh         hell01.java  2b.sh          1            sample1.txt  .rr.c.swp   sjf.c         env.c             uppercase_filenames.sh  priority.c.save    salary.sh
-.6a.c.swp     two.java     twoo.sh        .6a.c.swo    one.class    priority.c  world1.py     s.txt             help.sh                 a.out
-sample.txt    .            power.sh       hello1.java  new          6a.c        filenames.sh  multipleopens.sh  world.py                priority.c.save.1
+[22695a3203@GDLC 22695A3203]$ cc 6c.c
+[22695a3203@GDLC 22695A3203]$ ./a.out
+welcome
+child :114290
+\       2b,sh  6c     evenodd.sh    hell01.java  multipleopens.sh  permission.sh    priority.c.save.1  sample.txt  two.java                world.py
+1       2b.sh  6c.c   factorial.sh  hello1.java  new               power.sh         rr.c               sjf.c       twoo.sh
+10.c    6a.c   a.out  fcfs.c        hello.c      one.class         priority.c       salary.sh          s.txt       uppercase_filenames.sh
+123.sh  6b.c   env.c  filenames.sh  help.sh      one.sh            priority.c.save  sample1.txt        two.class   world1.py
+parent :114289
+factorial.sh  \            permission.sh  6b.c         sample1.txt  priority.c  filenames.sh  multipleopens.sh        one.sh             6c
+2b,sh         fcfs.c       2b.sh          1            one.class    6a.c        123.sh        .priority.c.swp         priority.c.save    salary.sh
+.6a.c.swp     hell01.java  twoo.sh        .6a.c.swo    new          evenodd.sh  env.c         uppercase_filenames.sh  a.out
+6c.c          two.java     power.sh       hello1.java  rr.c         sjf.c       10.c          help.sh                 priority.c.save.1
+sample.txt    .            ..             hello.c      .rr.c.swp    world1.py   s.txt         world.py                two.class
+
 
